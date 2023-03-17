fd84ad9c2d953d0e8ef09fad7903223557707199
Ronan <ronangray998@gmail.com>
2023-03-17 18:58:46 +0800

    Multiplayer Progress
    Split screen, joining, done just need to figure out assigning the players to separate layers as they join

--- Assets/Code/Script/Multiplayer System/PlayerManager.cs
+++ Assets/Code/Script/Multiplayer System/PlayerManager.cs
@@ -4,28 +4,12 @@ using UnityEngine;
 using Cinemachine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerManager : MonoBehaviour
 {
     private List<PlayerInput> players = new List<PlayerInput>();
     [SerializeField] private List<Transform> startingPoints;
     [SerializeField] private List<LayerMask> playerLayers;
-
-    private PlayerInputManager playerInputManager;
-
-    private void Awake()
-    {
-        playerInputManager = FindObjectOfType<PlayerInputManager>();
-    }
-
-    private void OnEnable()
-    {
-        playerInputManager.onPlayerJoined += AddPlayer;
-    }
-
-    private void OnDisable()
-    {
-        playerInputManager.onPlayerJoined -= AddPlayer;
-    }
 
     public void AddPlayer(PlayerInput player)
     {
