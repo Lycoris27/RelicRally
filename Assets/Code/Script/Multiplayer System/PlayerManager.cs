a4e91f0a1b7cea32431f10599ca29124d627a699
Ronan <ronangray998@gmail.com>
2023-03-17 16:53:39 +0800

    Multiplayer Almost working

--- Assets/Code/Script/Multiplayer System/PlayerManager.cs
+++ Assets/Code/Script/Multiplayer System/PlayerManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<PlayerInput> players = new List<PlayerInput>();
+    [SerializeField] private List<Transform> startingPoints;
+    [SerializeField] private List<LayerMask> playerLayers;
+
+    private PlayerInputManager playerInputManager;
+
+    private void Awake()
     {
-        
+        playerInputManager = FindObjectOfType<PlayerInputManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerInputManager.onPlayerJoined += AddPlayer;
+    }
+
+    private void OnDisable()
+    {
+        playerInputManager.onPlayerJoined -= AddPlayer;
+    }
+
+    public void AddPlayer(PlayerInput player)
+    {
+        players.Add(player);
+
+        Transform playerParent = player.transform.parent;
+        playerParent.transform.position = startingPoints[players.Count - 1].position;
+
+        int layerToAdd = (int)Mathf.Log(playerLayers[players.Count - 1].value, 2);
+
+
+        playerParent.GetComponentInChildren<CinemachineVirtualCamera>().gameObject.layer = layerToAdd;
+        playerParent.GetComponentInChildren<Camera>().cullingMask |= 1 << layerToAdd;
     }
 }
